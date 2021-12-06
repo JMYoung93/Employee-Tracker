3156d06eb7d53e3e23ce8cf9ce9dfd99ca6b5d8c
Jeffrey <hernew@gmail.com>
2021-12-06 06:43:44 -0800

    seeds and schema updated

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -14,14 +14,16 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL NOT NULL,
     department_id INT,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT DEFAULT NULL,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
