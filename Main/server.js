bb80b8b43a979aa11065dc3504ea619d1fa66490
Jeffrey <hernew@gmail.com>
2021-12-06 10:30:39 -0800

    added view all employees function

--- Main/server.js
+++ Main/server.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-
+require("console.table");
 const mysql = require('mysql2')
 
 app.use(express.urlencoded({ extended: false }));
@@ -76,6 +76,8 @@ switch (answer.userSelect){
     case 'Update Employee\'s Salary':
             updatePay();
             break;
+};
+function viewEmployee() {
+    database.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id`);
+    console.table(viewEmployee);
 }
-
-    
