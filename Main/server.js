07085e4f26c39932c0451662b1162221c3c4fec3
Jeffrey <hernew@gmail.com>
2021-12-06 09:38:16 -0800

    added switch statement for CLI questions

--- Main/server.js
+++ Main/server.js
@@ -30,8 +30,52 @@ const db = mysql.createConnection(
         "View All Roles",
         "Add a Department",
         "Add a Role",
+        'Delete A Department',
+		'Delete A Role',
+		'Delete An Employee',
+        'Update A Role Salary',
+        'Update Employee\'s Manager',
+        'Update Employee\'s Salary'
       ]
   }];
 
-  
+switch (answer.userSelect){
+    case 'View All Employees':
+            viewEmployee();
+            break;
+    case 'Add an Employee':
+            addEmployee();
+            break;
+    case 'Update Employee Role':
+            updateRole();
+            break;
+    case 'View All Departments':
+            viewDepartment();
+            break;
+    case 'Add a Department':
+            addDepartment();
+            break;
+    case 'Add a Role':
+            addRole();
+            break;
+    case 'Delete A Department':
+            deleteDept();
+            break;
+    case 'Delete A Role':
+            deleteRole();
+            break;
+    case 'Delete An Employee':
+            deleteEmployee();
+            break;
+    case 'Update A Role Salary':
+            updateSalary();
+            break;
+    case 'Update Employee\'s Manager':
+            updateManager();
+            break;
+    case 'Update Employee\'s Salary':
+            updatePay();
+            break;
+}
+
     
