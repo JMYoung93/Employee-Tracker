377e90c505f92c1673e95cdec47d77f4ed869301
Jeffrey <hernew@gmail.com>
2021-12-07 16:28:18 -0800

    working on duplicate table issue

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-require("console.table");
+//require("console.table");
 const mysql = require('mysql2');
 
 // Connect to database
@@ -14,14 +14,14 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee tracker database.`)
   );
-  
-  db.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected as id" + connection.threadId);
-  });
+
+  db.connect(function(err){
+          if(err) throw err;
+          launchApp();
+  })
 
 function launchApp() {
-  const questionList = inquirer.prompt([{
+   inquirer.prompt([{
       type: "list",
       name: "userSelect",
       message: "Welcome to your employee database! Please select an option below.",
@@ -41,9 +41,10 @@ function launchApp() {
         'Update Employee\'s Salary',
         'Exit'
       ]
-  }]);
 
-switch (answer.userSelect){
+  }]).then( (answer) =>{
+          console.log(answer)
+ switch (answer.userSelect) {
     case 'View All Employees':
             viewEmployee();
             break;
@@ -82,18 +83,76 @@ switch (answer.userSelect){
             break;
     case 'Exit':
             console.log('Goodbye!')
-            connection.end();
+            db.end();
             break;
-};
+}});
 function viewEmployee() {
     let query = 'SELECT * FROM employee';
-    connection.query(query, function(err, res) {
+    db.query(query, function(err, res) {
     if(err) throw err;
     console.log(res.length + ' employees are currently employed!');
     console.table('Here are all employees:', res);
     launchApp();
 })
 }
+
+function addEmployee(){
+        db.query('SELECT * FROM role', function (err, res){
+                if (err) throw err;
+                inquirer
+            .prompt([
+                {
+                    name: 'first_name',
+                    type: 'input', 
+                    message: "What is the employee's fist name? ",
+                },
+                {
+                    name: 'last_name',
+                    type: 'input', 
+                    message: "What is the employee's last name? "
+                },
+                {
+                    name: 'manager_id',
+                    type: 'input', 
+                    message: "What is the employee's manager's ID? "
+                },
+                {
+                    name: 'role', 
+                    type: 'list',
+                    choices: function() {
+                    var roleArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        roleArray.push(res[i].title);
+                    }
+                    return roleArray;
+                    },
+                    message: "What is this employee's role? "
+                }
+                ]).then(function (answer) {
+                    let role_id;
+                    for (let a = 0; a < res.length; a++) {
+                        if (res[a].title == answer.role) {
+                            role_id = res[a].id;
+                            console.log(role_id)
+                        }                  
+                    }  
+                    connection.query(
+                    'INSERT INTO employee SET ?',
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        manager_id: answer.manager_id,
+                        role_id: role_id,
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        console.log('Your employee has been added!');
+                        options();
+                    })
+                })
+        })
+}; 
+        
 };
 launchApp();
  //database.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id`);
